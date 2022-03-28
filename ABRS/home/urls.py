e44040a17cd480984088acfb5d582f1bede144b6
sudheer119 <76642126+sudheer119@users.noreply.github.com>
2022-03-28 10:54:21 +0530

    Delete ABRS directory
    Due Major changes in the code deleted the directory

--- ABRS/home/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.home),
-    path('author/', views.Author.as_view()),
-    path('home/add/', views.add),
-]
--- ABRS/notes/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Notes(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
